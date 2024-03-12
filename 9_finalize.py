72c533384de611a26c7888981e69ee46971e8a5f
gormlabenz <gorm@labenz.io>
2024-03-12 17:27:22 -0400

    Refactor image handling and add image mapping to TypeScript file

--- 9_finalize.py
+++ 9_finalize.py
@@ -2,14 +2,23 @@ import json
 import re
 
 
+def get_basename_from_url(url):
+    """
+    Extrahiert den Basenamen aus der URL, ohne den Namen zu verändern.
+    """
+    # Extrahiert den letzten Teil der URL nach dem letzten '/' als Dateinamen
+    file_name = url.split('/')[-1]
+    # Entfernt die Dateiendung, wenn vorhanden, ohne den Namen zu verändern
+    base_name = file_name.rsplit('.', 1)[0]
+    return base_name
+
+
 def adjust_image_objects(image_obj):
-    # Anpassung der Image-Objekte
     new_image_obj = {}
     if 'new_caption' in image_obj:
         new_image_obj['caption'] = image_obj['new_caption']
     if 'file' in image_obj:
-        base_name = re.sub(r'File:', '', image_obj['file']).rsplit('.', 1)[0]
-        new_image_obj['url'] = f"/assets/images/{base_name}.png"
+        new_image_obj['file'] = get_basename_from_url(image_obj['url'])
     return new_image_obj
 
 
@@ -17,23 +26,35 @@ def process_json(input_file, output_file):
     with open(input_file, 'r', encoding='utf-8') as file:
         data = json.load(file)
 
+    images_dict = {}
+
     for date, details in data.items():
-        # Entfernung des raw Entries aus den Events
-        for event in details.get('events', []):
-            if 'raw' in event:
-                del event['raw']
+        if 'images' in details:
+            for image in details['images']:
+                if not image['file'].lower().endswith('.svg'):
+                    images_dict[get_basename_from_url(
+                        image['url'])] = f"../assets/images/{get_basename_from_url(image['url'])}.png"
 
-        # Entfernung aller Image Entries, bei denen das Bild ein SVG ist
-        images = details.get('images', [])
-        images = [img for img in images if not img['file'].lower().endswith('.svg')]
-
-        # Anpassung der Image Objekte
-        adjusted_images = [adjust_image_objects(img) for img in images]
+        # Anpassungen hier...
+        image_objects = details.get('images', [])
+        image_objects = [
+            img for img in image_objects if not img['file'].lower().endswith('.svg')]
+        adjusted_images = [adjust_image_objects(img) for img in image_objects]
         details['images'] = adjusted_images
 
     with open(output_file, 'w', encoding='utf-8') as file:
         json.dump(data, file, indent=4, ensure_ascii=False)
 
+    with open('Images.ts', 'w') as ts_file:
+        ts_file.write('const images = {\n')
+        for key, url in images_dict.items():
+            if """'""" in key:
+                key_string = f'"{key}"'
+            else:
+                key_string = f"'{key}'"
+            ts_file.write(f"  {key_string}: require(\"{url}\"),\n")
+        ts_file.write('};\n\nexport default images;\n')
+
 
 if __name__ == "__main__":
-    process_json('captions.json', 'output.json')
+    process_json('categories.json', 'data.json')
